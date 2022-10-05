c2c485195a6b5bcc783b73d984c8d718a3291ac7
Shashank Kothari <marutikothari39@gmail.com>
2022-10-05 20:51:40 +0530

    P3

--- src/components/About.js
+++ src/components/About.js
@@ -75,7 +75,7 @@ let myStyle={
   <div className="accordion-item">
     <h2 className="accordion-header" id="headingThree">
       <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
+        Restaurant
       </button>
     </h2>
     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
